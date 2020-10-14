fbe3862f238d9c161f1ccbf5006fc08378554737
susanisaksson <susan.isaksson@medieinstitutet.se>
2020-10-14 11:32:39 +0200

    Skapat html-filer, javaScript och css

---
+++ javaScript/main.js
@@ -0,0 +1,15 @@
+function buttonRed () {
+    document.body.style.backgroundColor = "red";
+}
+
+function buttonGreen () {
+    document.body.style.backgroundColor = "green";
+}
+
+function buttonBlue () {
+    document.body.style.backgroundColor = "blue";
+}
+
+function buttonBlack () {
+    document.body.style.backgroundColor = "black";
+}
